a1d97b8bb69da0ac7d28490e6dd9af1b5c1aeafc
Roger Ximenez <rogerximenez@Rogers-MBP.lan>
2022-06-06 16:39:14 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 const weatherForm = document.querySelector('#weather-form')
 const search = document.querySelector('.search-input')
 const messageOne = document.querySelector('#message-1')
@@ -22,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 messageOne.textContent = data.error
